951827f1eebaf33224b2639c3e92c6a524776fcc
wozniaklukasz <wozniak.lukasz.g@gmail.com>
2021-04-01 13:47:39 +0000

    Deploying to gh-pages from @ wozniaklukasz/blog-deploy-nextjs-app-to-github-pages-using-github-actions@500e05f28f6dbfdd786e00c9a5a0ac193a3b015c 🚀

--- _next/static/DoOAy5mPc1Q5Xxlkggnny/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-3a71a045ae0e0d05d34c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f25ce16553a0f80675c1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/Qd0YUj4985KdzFrpDPBh1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-88b90b9eb4dcae7bbc86.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f25ce16553a0f80675c1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
